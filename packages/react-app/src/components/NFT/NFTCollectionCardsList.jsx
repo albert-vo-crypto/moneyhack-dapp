65484f777e438263718a7ad09cc7e178a8725b73
Sing <16409791+singyiu@users.noreply.github.com>
2022-05-15 13:52:54 -0700

    NFTCollectionCardsList sizing

--- packages/react-app/src/components/NFT/NFTCollectionCardsList.jsx
+++ packages/react-app/src/components/NFT/NFTCollectionCardsList.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 import NFTCollectionCard from "./NFTCollectionCard";
 
-const NFTCollectionCardsList = ({ nftCollections, gridCols = 4 }) => {
+const NFTCollectionCardsList = ({ nftCollections }) => {
   const ulClassString =
-    "mx-10 my-10 grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-" + gridCols + " xl:gap-x-8";
+    "mx-10 my-10 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-5 gap-x-4 gap-y-8 sm:gap-x-6 xl:gap-x-8";
   return (
     <div class={ulClassString}>
       {nftCollections.map(coll => (
