e6d7f51f949f774f6c8f7ad7f06af6f09b4be406
Sing <16409791+singyiu@users.noreply.github.com>
2022-05-15 13:59:26 -0700

    HeaderText sizing

--- packages/react-app/src/views/ExploreView.jsx
+++ packages/react-app/src/views/ExploreView.jsx
@@ -1,14 +1,11 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React from "react";
+import { useSelector } from "react-redux";
 
-//import { reloadBidableCollectionsAction } from "../stores/reducers/nft";
 import { activeBidableNftCollectionsSelector } from "../stores";
 import NFTCollectionCardsList from "../components/NFT/NFTCollectionCardsList";
 import HeaderText from "../components/Commons/HeaderText";
 
 const ExploreView = () => {
-  //const dispatch = useDispatch();
-
   const nftCollections = useSelector(activeBidableNftCollectionsSelector);
 
   return (
