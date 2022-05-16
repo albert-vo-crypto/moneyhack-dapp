c838ecdf3106d8a45c38b2c8adce34dfdeb5bf71
Gunvant Kathrotiya <gunvantk@users.noreply.github.com>
2022-05-15 22:09:16 -0700

    Added enums and some validation

--- packages/hardhat/contracts/RBFVault.sol
+++ packages/hardhat/contracts/RBFVault.sol
@@ -7,11 +7,21 @@ import "./PaymentSplitter.sol";
 
 interface ICollectionContract {
     function transferOwnership(address newOwner) external;
+
+    function owner() public view returns (address);
 }
 
 contract RBFVault is PaymentSplitter {
+    enum Status {
+        Initialized,
+        Active,
+        Expired,
+        Canceled
+    }
+
     address public collectionOwner;
     address public collectionAddress;
+    Status public status;
 
     constructor(
         address _collectionAddress,
@@ -20,11 +30,31 @@ contract RBFVault is PaymentSplitter {
     ) PaymentSplitter(_parties, _shares) {
         collectionAddress = _collectionAddress;
         collectionOwner = _parties[1];
+        status = Status.Initialized;
     }
 
-    function returnOwnershipToCollectionOwner() external {
+    modifier termsSatisfied() {
+        // check if contract time-length completed
+        // check if revenue max limit has reached
+        _;
+    }
+
+    function returnOwnershipToCollectionOwner() external termsSatisfied {
         ICollectionContract(collectionAddress).transferOwnership(
             collectionOwner
         );
+    }
+
+    function isVaultOwnsTheCollection() public returns (bool) {
+        return ICollectionContract(collectionAddress).owner() == address(this);
+    }
+
+    function Activate() external {
+        // TODO - verify collection payout address using oracle
+        require(
+            isVaultOwnsTheCollection(),
+            "Vault: Collection isn't owned by vault"
+        );
+        status = Status.Active;
     }
 }
