a8078f56f41323ad1b069b5424a8a4602ab47f46
Gunvant Kathrotiya <gunvantk@users.noreply.github.com>
2022-05-20 13:58:50 -0700

    Updated theme to light, text label

--- packages/react-app/src/components/Commons/HeaderText.jsx
+++ packages/react-app/src/components/Commons/HeaderText.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HeaderText = ({ children }) => {
   return (
-    <h1 className="flex-1 text-2xl font-bold text-gray-900 mt-5">  {children}</h1>
+    <h1 className="flex-1 text-3xl font-extrabold text-gray-900 mt-5">  {children}</h1>
     
   );
 };
