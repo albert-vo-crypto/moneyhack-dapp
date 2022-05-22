d6ec312a9c733e2119a6e5947153b259417fc403
Sing <16409791+singyiu@users.noreply.github.com>
2022-05-22 08:49:43 -0700

    updated tag line

--- packages/react-app/src/views/Home.jsx
+++ packages/react-app/src/views/Home.jsx
@@ -23,10 +23,10 @@ function Home({ yourLocalBalance, readContracts }) {
         <div className="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
           <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
             <span className="block xl:inline">A better way to</span>{" "}
-            <span className="block text-indigo-600 xl:inline">Raise capital</span>
+            <span className="block text-indigo-600 xl:inline">raise capital</span>
           </h1>
           <p className="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">
-            Revefin transforms recurring revenue into up-front capital for growth without dilution or restrictive debt.
+            ReveFin turns smart contract revenue into a tradeable asset. Trade future revenue for upfront capital.
           </p>
           <div className="mt-10 sm:flex sm:justify-center lg:justify-start">
             <div className="rounded-md shadow">
