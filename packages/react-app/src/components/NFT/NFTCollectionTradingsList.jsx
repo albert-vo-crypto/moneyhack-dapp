0d9c47bbdab245794bfa0fc246c4617181ebbe12
Gunvant Kathrotiya <gunvantk@users.noreply.github.com>
2022-05-21 21:43:48 -0700

    Updated accept offer button

--- packages/react-app/src/components/NFT/NFTCollectionTradingsList.jsx
+++ packages/react-app/src/components/NFT/NFTCollectionTradingsList.jsx
@@ -139,19 +139,25 @@ const NFTCollectionTradingsList = ({ nftCollections, ethPrice, opMode = "creator
       render: (value, nftCollection) => {
         if (nftCollection && nftCollection.bidDetails && nftCollection.bidDetails.length > 0 && !shouldHideAction) {
           return opMode === "creator" ? (
-            <SecondaryButton
+            <button
+              type="button"
+              className="bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               onClick={() => {
                 goToAcceptBidNextSteps(nftCollection);
               }}
-              children={"Accept Bid Offer"}
-            />
+            >
+              Accept offer
+            </button>
           ) : (
-            <HighlightButton
-              onClick={() => {
+            <button
+            type="button"
+            className="bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+             onClick={() => {
                 //TODO: to be implemented
               }}
-              children={"Withdraw Bid Offer"}
-            />
+              >
+              Withdraw Offer
+            </button>
           );
         } else {
           return <h3></h3>;
